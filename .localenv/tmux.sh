d9377fbac28028ebec738dd1b7ef60cbbc422997
MHarri Tran <humanhoang@proton.me>
2025-10-20 03:07:42 +0700

    sun20

---
+++ .localenv/tmux.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if tmux has-session -t BLG1 2>/dev/null; then
+  tmux attach -t BLG1
+
+else
+  tmux new -d -s BLG1
+  tmux send-keys -t BLG1 "cd ~/REPO1/BLG1/app1" C-m
+  # tmux attach -t BLG1
+  tmux ls
+fi
