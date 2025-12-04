c45a2477b1152fe63dc9737598266a57444e6c68
MHarri Tran <humanhoang@proton.me>
2025-12-05 03:20:18 +0700

    dec5

--- .localenv/tmux.sh
+++ .localenv/tmux.sh
@@ -5,7 +5,7 @@ if tmux has-session -t BLG1 2>/dev/null; then
 
 else
   tmux new -d -s BLG1
-  tmux send-keys -t BLG1 "cd ~/REPO1/BLG1/app1" C-m
+  tmux send-keys -t BLG1 "cd ~/REPO1/BLG1/app1; npm run dev" C-m
   # tmux attach -t BLG1
   tmux ls
 fi
